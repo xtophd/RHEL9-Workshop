0b5faa544bc5d2c8c9e9f5b619eabb0d7f0aba8b
xtophd <christoph@linuxsoup.com>
2022-06-07 10:32:03 -0400

    - first commit after porting rhel8 workshop source to rhel9 - docs have NOT yet been updated to reflect rhel9 - only testing with deployment platform is OVIRT thus far

---
+++ documentation/_include/_images/README.txt
@@ -0,0 +1,27 @@
+## Put images for documentation here
+
+## 2019
+# I used imagemagick to crop the screenshots
+
+    magick convert test.png -crop 1024x768+448+143 new.png   
+
+# The WINDOWS/CMD/DOS cmdline version in a "for loop"
+# This will put a cropped image of the same filename in a directory ".\crops"
+
+    FOR %a in (*.png) DO magick convert "%a" -crop 1024x768+448+143 ".\crops\%a"
+  
+## 2020 - Display size 1920x1200
+
+    magick convert test.png -crop  1920x1160+0+72 new.png
+    
+    FOR %a in (*.png) DO magick convert "%a" -crop 1920x1160+0+72  ".\crops\%a"
+
+    Then used PowerPoint to annotate the images and export to PNG
+
+## 2021 - Display size 2560x1440
+
+    magick convert test.png -crop  2560x1330+0+70 new.png
+    
+    FOR %a in (*.png) DO magick convert "%a" -crop 2560x1330+0+70  ".\crops\%a"
+
+    Then used PowerPoint to annotate the images and export to PNG
---
+++ xtoph-deploy.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+##
+## NOTE: you must point to the correct inventory and extravars yml
+##
+##   Take a sample configs from ./sample-configs and 
+##   copy them to ./playbooks/config
+##
+
+myInventory="./config/master-config.yml"
+
+if [[ ! -e "${myInventory}" || ! -e "./xtoph-deploy.yml" ]] ; then
+    echo "ERROR: Are you in the right directory? Can not find ${myInventory} | xtoph-deploy.yml" ; exit
+    exit
+fi
+
+##
+##
+##
+
+if [[ $2 != "" ]]; then
+
+    echo "Ansible limit set to: $2"
+
+    myLimits="-l $2"
+
+else
+
+    myLimits=""
+
+fi
+
+case "$1" in
+
+    "deploy"     | \
+    "undeploy"   | \
+    "redeploy"   | \
+    "workshop"   | \
+    "setup+"     | \
+    "setup")
+
+        time  ansible-playbook --ask-vault-pass -i ${myInventory} -f 10 -e xtoph_deploy_cmd=${1} ${myLimits} xtoph-deploy.yml
+        ;;
+
+    "git-updates")
+
+        git pull
+        cd roles/xtoph_deploy; git pull
+        ;;
+
+    *)
+        echo "USAGE: xtoph-deploy.sh [ setup | setup+ | deploy | undeploy | redeploy | workshop ]"
+        echo ""
+        echo "  setup     ... runs only 'setup' plays"
+        echo "  setup+    ... runs both 'setup' and 'deploy' plays"
+        echo "  deploy    ... runs only 'deploy' plays"
+        echo "  undeploy  ... runs only 'undeploy' plays"
+        echo "  redeploy  ... runs both 'undeploy' and 'deploy' plays"
+        echo "  workshop  ... runs only the 'workshop' plays" 
+        ;;
+
+esac         
